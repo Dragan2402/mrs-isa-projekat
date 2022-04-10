b36db3f1e337a5ad514df9312354b5e59fc48680
Dragan2402 <63748840+Dragan2402@users.noreply.github.com>
2022-04-10 20:14:51 +0200

    #1 Kreiran kostur projekta
    Kreiran kostur projekta, inicijalizovan spring sa HelloWorld kontrolerom. Potrebno prilagoditi postgres username i sifru

---
+++ projekat/src/main/java/com/projekat/projekat_mrs_isa/ProjekatMrsIsaApplication.java
@@ -0,0 +1,13 @@
+package com.projekat.projekat_mrs_isa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatMrsIsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjekatMrsIsaApplication.class, args);
+    }
+
+}
---
+++ projekat/src/test/java/com/projekat/projekat_mrs_isa/ProjekatMrsIsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekat.projekat_mrs_isa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekatMrsIsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
