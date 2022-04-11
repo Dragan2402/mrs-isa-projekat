ee8a997c9af8d9b60c56c0c69e0d19120d908ca5
Dragan2402 <63748840+Dragan2402@users.noreply.github.com>
2022-04-11 22:34:34 +0200

    Dodat spisak metoda za Client Service

--- projekat/src/main/java/com/projekat/projekat_mrs_isa/service/ClientService.java
+++ projekat/src/main/java/com/projekat/projekat_mrs_isa/service/ClientService.java
@@ -1,11 +1,30 @@
 package com.projekat.projekat_mrs_isa.service;
 
+import com.projekat.projekat_mrs_isa.model.Client;
 import com.projekat.projekat_mrs_isa.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
+
+    public Client findById(long id) { return clientRepository.findById(id).orElse(null) ;}
+
+    public List<Client> findAll() { return clientRepository.findAll(); }
+
+    public Page<Client> findAll(Pageable page) { return clientRepository.findAll(page);}
+
+    public Client save(Client client) { return clientRepository.save(client); }
+
+    public void remove(long id) {  clientRepository.deleteById(id);}
+
+    public Client findByEmail(String email) { return  clientRepository.findByEmail(email); }
+
+
 }
