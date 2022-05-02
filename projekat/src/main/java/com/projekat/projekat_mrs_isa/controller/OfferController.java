2c1157b1ec3b76bbc040c77cde97ea46c7959746
Dimitrije Petrov <dimpet96@gmail.com>
2022-05-02 21:45:47 +0200

    Dodat servis i repozitorijum za RentingEntity

--- projekat/src/main/java/com/projekat/projekat_mrs_isa/controller/OfferController.java
+++ projekat/src/main/java/com/projekat/projekat_mrs_isa/controller/OfferController.java
@@ -4,10 +4,8 @@ package com.projekat.projekat_mrs_isa.controller;
 import com.projekat.projekat_mrs_isa.dto.OfferDTO;
 import com.projekat.projekat_mrs_isa.model.Offer;
 import com.projekat.projekat_mrs_isa.model.RentingEntity;
-import com.projekat.projekat_mrs_isa.service.FishingClassService;
 import com.projekat.projekat_mrs_isa.service.OfferService;
-import com.projekat.projekat_mrs_isa.service.ShipService;
-import com.projekat.projekat_mrs_isa.service.VacationHouseService;
+import com.projekat.projekat_mrs_isa.service.RentingEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,21 +24,13 @@ public class OfferController {
     private OfferService offerService;
 
     @Autowired
-    private VacationHouseService vacationHouseService;
-
-    @Autowired
-    private ShipService shipService;
-
-    @Autowired
-    private FishingClassService fishingClassService;
+    private RentingEntityService rentingEntityService;
 
     @PostMapping(value = "/new", consumes = MediaType.APPLICATION_JSON_VALUE)
     @Transactional
     public ResponseEntity<OfferDTO> createOffer(@RequestBody OfferDTO offerDTO) {
         Long rentingEntityId = offerDTO.getRentingEntityId();
-        RentingEntity rentingEntity = vacationHouseService.findById(rentingEntityId);
-        if (rentingEntity == null) rentingEntity = shipService.findById(rentingEntityId);
-        if (rentingEntity == null) rentingEntity = fishingClassService.findById(rentingEntityId);
+        RentingEntity rentingEntity = rentingEntityService.findById(rentingEntityId);
         if (rentingEntity == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         Offer newOffer = new Offer(offerDTO, rentingEntity);
         offerService.save(newOffer);
