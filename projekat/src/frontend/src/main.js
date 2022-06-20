30df1a4a31762be3912852820a79ade8230b782b
Dragan2402 <63748840+Dragan2402@users.noreply.github.com>
2022-06-20 19:57:11 +0200

    Update main.js

--- projekat/src/frontend/src/main.js
+++ projekat/src/frontend/src/main.js
@@ -86,7 +86,7 @@ const routes = [
 ];
 
 const router = new createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
     routes
 })
 
