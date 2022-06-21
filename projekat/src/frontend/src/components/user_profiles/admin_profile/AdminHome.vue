dd290dbce96c49eb7612a0a3dbc8291d6a09f078
Andrej Čuljak <krtolinat@gmail.com>
2022-06-21 11:03:57 +0200

    Update AdminHome.vue

--- projekat/src/frontend/src/components/user_profiles/admin_profile/AdminHome.vue
+++ projekat/src/frontend/src/components/user_profiles/admin_profile/AdminHome.vue
@@ -1,34 +1,44 @@
 <template>
   <nav>
     <div class="nav nav-tabs justify-content-center mt-2 mb-3" id="nav-tab" role="tablist">
-      <button class="nav-link active nav-button" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button"
+      <button class="nav-link active nav-button" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-profile"
+              type="button"
               role="tab" aria-controls="nav-profile" aria-selected="true" @Click="routeProfile">Profile
       </button>
-      <button class="nav-link nav-button" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-entities" type="button"
+      <button class="nav-link nav-button" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-entities"
+              type="button"
               role="tab" aria-controls="nav-entities" aria-selected="false" @Click="routeEntities">Entities
       </button>
-      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-reviews" type="button"
+      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-reviews"
+              type="button"
               role="tab" aria-controls="nav-reviews" aria-selected="false" @Click="routeReviews">Reviews
       </button>
-      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-complaints" type="button"
+      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-complaints"
+              type="button"
               role="tab" aria-controls="nav-complaints" aria-selected="false" @Click="routeComplaints">Complaints
       </button>
-      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-requests" type="button"
+      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-requests"
+              type="button"
               role="tab" aria-controls="nav-requests" aria-selected="false" @Click="routeRequests">Requests
       </button>
-      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-reports" type="button"
+      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-reports"
+              type="button"
               role="tab" aria-controls="nav-reports" aria-selected="false" @Click="routeReports">Reports
       </button>
-      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-register" type="button"
+      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-register"
+              type="button"
               role="tab" aria-controls="nav-register" aria-selected="false" @Click="routeRegister">Register
       </button>
-      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-fee" type="button"
+      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-fee"
+              type="button"
               role="tab" aria-controls="nav-fee" aria-selected="false" @Click="routeFee">Fee
       </button>
       <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-reservations"
-              type="button" role="tab" aria-controls="nav-reservations" aria-selected="false" @Click="routeReservations">Reservations
+              type="button" role="tab" aria-controls="nav-reservations" aria-selected="false"
+              @Click="routeReservations">Reservations
       </button>
-      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-loyalty" type="button"
+      <button class="nav-link nav-button" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-loyalty"
+              type="button"
               role="tab" aria-controls="nav-loyalty" aria-selected="false" @Click="routeLoyalty">Loyalty
       </button>
     </div>
@@ -73,9 +83,7 @@
 export default {
   name: "AdminProfile",
   data() {
-    return {
-      chosenRoute: null
-    }
+    return {}
   },
 
   mounted() {
@@ -119,7 +127,7 @@ export default {
       this.$router.push({name: "adminReservations"});
     },
 
-    routeLoyalty () {
+    routeLoyalty() {
       this.$router.push({name: "adminLoyalty"});
     }
   }
